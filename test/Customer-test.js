f4538b65d61853fdd7109d92eddabc47e8e85c21
Lindsay Schnell <lschnell8@gmail.com>
2022-01-15 20:27:14 -0700

    Initial commit

---
+++ test/Customer-test.js
@@ -0,0 +1,50 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Customer', () => {
+
+  it('Should be a function', () => {
+    expect(Customer).to.be.a('function');
+  });
+
+  it('Should be an instance of Customer', () => {
+    expect(customer).to.be.an.instanceOf(Customer);
+  });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+  // it('Should', () => {
+  //   expect().to.equal();
+  // });
+
+});
