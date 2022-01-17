d5202dd5ba631b1e9d6f096032a97d3ecc963d70
lschnell8 <lschnell8@gmail.com>
2022-01-16 19:38:32 -0700

    Separate data for edgecase dataset

--- src/sampleData/bookingsData.js
+++ src/sampleData/bookingsData.js
@@ -29,35 +29,4 @@ const bookings = [
   }
 ];
 
-// const bookings = [
-//   {
-//     "id": "5fwrgu4i7k55hl6t8",
-//     "userID": 1,
-//     "date": "2022/01/11",
-//     "roomNumber": 1,
-//     "roomServiceCharges": []
-//   },
-//   {
-//     "id": "5fwrgu4i7k55hl6x8",
-//     "userID": 1,
-//     "date": "2022/01/11",
-//     "roomNumber": 2,
-//     "roomServiceCharges": []
-//   },
-//   {
-//     "id": "5fwrgu4i7k55hl6uf",
-//     "userID": 2,
-//     "date": "2022/01/11",
-//     "roomNumber": 3,
-//     "roomServiceCharges": []
-//   },
-//   {
-//     "id": "5fwrgu4i7k55hl6t6",
-//     "userID": 13,
-//     "date": "2022/01/11",
-//     "roomNumber": 4,
-//     "roomServiceCharges": []
-//   }
-// ];
-
 module.exports = bookings
