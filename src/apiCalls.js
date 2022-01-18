ee0f0f846024fe55fe6d161a0b04e614cca179b7
lschnell8 <lschnell8@gmail.com>
2022-01-18 00:02:00 -0700

    Update apiCall to pull a single customer

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,20 @@
-export const fetchApiData = (type) => {
-  return fetch(`http://localhost:3001/api/v1/${type}`)
-    .then((response) =>
-      response.json())
-    .catch(error => console.log('fetch error', error))
-};
-
-// export const fetchApiData = (type, id) => {
-//   if (!id) {
-//     return fetch(`http://localhost:3001/api/v1/${type}`)
-//       .then((response) =>
-//         response.json())
-//       .catch(error => console.log('fetch error', error))
-//   } else {
-//     return fetch(`http://localhost:3001/api/v1/${type}/${id}`)
-//       .then((response) =>
-//         response.json())
-//       .catch(error => console.log('fetch error', error))
-//   }
+// export const fetchApiData = (type) => {
+//   return fetch(`http://localhost:3001/api/v1/${type}`)
+//     .then((response) =>
+//       response.json())
+//     .catch(error => console.log('fetch error', error))
 // };
+
+export const fetchApiData = (type, id) => {
+  if (!id) {
+    return fetch(`http://localhost:3001/api/v1/${type}`)
+      .then((response) =>
+        response.json())
+      .catch(error => console.log('fetch error', error))
+  } else {
+    return fetch(`http://localhost:3001/api/v1/${type}/${id}`)
+      .then((response) =>
+        response.json())
+      .catch(error => console.log('fetch error', error))
+  }
+};
