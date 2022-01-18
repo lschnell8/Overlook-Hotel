e6f700161747011a166e23201b221a9eadda6472
lschnell8 <lschnell8@gmail.com>
2022-01-17 22:05:28 -0700

    apiCall working

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,3 +4,17 @@ export const fetchApiData = (type) => {
       response.json())
     .catch(error => console.log('fetch error', error))
 };
+
+// export const fetchApiData = (type, id) => {
+//   if (!id) {
+//     return fetch(`http://localhost:3001/api/v1/${type}`)
+//       .then((response) =>
+//         response.json())
+//       .catch(error => console.log('fetch error', error))
+//   } else {
+//     return fetch(`http://localhost:3001/api/v1/${type}/${id}`)
+//       .then((response) =>
+//         response.json())
+//       .catch(error => console.log('fetch error', error))
+//   }
+// };
--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,7 @@ import { fetchApiData } from './apiCalls';
 import BookingsLog from './classes/BookingsLog';
 import RoomListings from './classes/RoomListings';
 import Customer from './classes/Customer';
+// import './domUpdates.js';
 
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
@@ -12,25 +13,33 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//GLOBAL VARIABLES
 let bookingsLog, roomListings, customer;
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+//EXECUTION FUNCTIONS
 const loadData = () => {
   getData()
-  .then(instantiateClasses())
+    .then(data => instantiateClassInstances(data))
 };
 
+
+//HELPER FUNCTIONS
 const getData = () => {
   return Promise.all([fetchApiData('bookings'), fetchApiData('rooms'), fetchApiData('customers')])
 };
 
-const instantiateClasses = () => {
-  (data) => {
+const instantiateClassInstances = (data) => {
     bookingsLog = new BookingsLog(data[0]);
     roomListings = new RoomListings(data[1]);
     let customerListing = data[2];
     customer = new Customer(customerListing.customers[0]);
-  }
+    // let customer2 = new Customer(customer.id)
+    console.log(bookingsLog, roomListings, customer);
 };
 
-window.addEventListener('load', loadData())
+
+
+
+//EVENT LISTENERS
+window.addEventListener('load', loadData());
