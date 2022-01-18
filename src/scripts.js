bda7ce13181d1359a6eeeb1da0127e4abbd8f604
lschnell8 <lschnell8@gmail.com>
2022-01-18 00:02:47 -0700

    Add querySelectors and login funtion

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@ import { fetchApiData } from './apiCalls';
 import BookingsLog from './classes/BookingsLog';
 import RoomListings from './classes/RoomListings';
 import Customer from './classes/Customer';
-// import './domUpdates.js';
+import './domUpdates.js';
 
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
@@ -13,29 +13,61 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//QUERY SELECTORS
+
+//BUTTONS
+const logInBtn = document.getElementById('logInBtn');
+// const findAvailableRoomsBtn = document.getElementById('findAvailableRoomsBtn');
+// const bookMyRoomBtn = document.getElementById('bookMyRoomBtn');
+
+//INPUTS
+const userNameInput = document.getElementById('userNameInput').value;
+// const passwordInput = document.getElementById('passwordInput');
+// const dateInput = document.getElementById('dateInput');
+// const roomTypeSelection = document.getElementById('roomTypeSelection');
+
+//PAGE VIEWS
+// const dashboard = document.getElementById('dashboard');
+// const availableRooms = document.getElementById('availableRooms');
+// const bookingARoom = document.getElementById('bookingARoom');
+
+//TEXT //Don't need these can insertAdjacent HTML afterbegin dashboard
+// const greeting = document.getElementById('greeting');
+// const amountSpent = document.getElementById('amountSpent');
+
+
+//roomTypeSelection.options[roomTypeSelection.selectedIndex].value; //This reads the value of the option in the select tag for roomType selection
+
+
 //GLOBAL VARIABLES
 let bookingsLog, roomListings, customer;
 
 
 //EXECUTION FUNCTIONS
 const loadData = () => {
-  getData()
-    .then(data => instantiateClassInstances(data))
+  
+};
+
+const logIn = () => {
+  let splitInput = userNameInput.split('r');
+  let id = Number(splitInput[1]);
+ getData(id)
+   .then(data => instantiateClassInstances(data))
 };
 
 
 //HELPER FUNCTIONS
-const getData = () => {
-  return Promise.all([fetchApiData('bookings'), fetchApiData('rooms'), fetchApiData('customers')])
+const getData = (id) => {
+  return Promise.all([fetchApiData('bookings'), fetchApiData('rooms'), fetchApiData('customers', id)])
 };
 
 const instantiateClassInstances = (data) => {
+  console.log(data)
     bookingsLog = new BookingsLog(data[0]);
     roomListings = new RoomListings(data[1]);
-    let customerListing = data[2];
-    customer = new Customer(customerListing.customers[0]);
-    // let customer2 = new Customer(customer.id)
-    console.log(bookingsLog, roomListings, customer);
+    // let customerListing = data[2];
+    // customer = new Customer(customerListing.customers[0]);
+    customer = new Customer(data[3])
 };
 
 
@@ -43,3 +75,4 @@ const instantiateClassInstances = (data) => {
 
 //EVENT LISTENERS
 window.addEventListener('load', loadData());
+logInBtn.addEventListener('onclick', logIn());
