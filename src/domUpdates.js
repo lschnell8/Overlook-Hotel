ac1f20cdaa119bd81962ba74af608da4c817efc4
lschnell8 <lschnell8@gmail.com>
2022-01-19 02:04:59 -0700

    Implement function to display available rooms

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,4 @@
-// import BookingsLog from './classes/BookingsLog';
-// import RoomListings from './classes/RoomListings';
-// import Customer from './classes/Customer';
-// const logInPage = document.getElementById('logIn');
-// let bookingsLog, roomListings, customer;
-
-
+import { logInForm } from './scripts.js'
 // const usernameInput = document.getElementById('usernameInput');
 // const logInForm = document.getElementById('logIn');
 const dashboard = document.getElementById('dashboard');
@@ -13,41 +7,44 @@ const availableRooms = document.getElementById('availableRooms');
 const bookingARoom = document.getElementById('bookingARoom');
 
 let domUpdates = {
+  
   //DISPLAY FUNCTIONS
-
+  
   displayInputError(inputType) { 
-    console.log(inputType)
-    let message = inputType.id.split('i')[0]
+    console.log(inputType.id)
+    let type = inputType.id.toString();
+    let message = type.split('i')[0]
     inputType.insertAdjacentHTML('beforebegin', `<p>*Please enter a valid ${message}*</p>`)
   },
-
-  displayDashboard(customer, bookingsLog, roomListings) {
+  
+  displayDashboard(customer, bookingsLog, roomListings, logInForm) {
     let amount = bookingsLog.calculateTotalSpent(roomListings, customer);
-    let customerBookings = bookingsLog.getCustomerBookings();
-
+    let customerBookings = bookingsLog.getCustomerBookings(customer);
     bookingsDisplay.insertAdjacentHTML('afterbegin',
-      `<h1>Hi ${customer.name}!</h1>
+    `<h1>Hi ${customer.name.split(' ')[0]}!</h1>
     <h3> My Bookings </h3>`);
     
     customerBookings.forEach(booking => {
       bookingsDisplay.insertAdjacentHTML('beforeend',
-        `<article>
-        <li>Date: ${booking.date}</li>
-        <li>Room Number: ${booking.roomNumber}</li>
-        <li>Confirmation Number: ${booking.id}</li>
+      `<article>
+      <li>Date: ${booking.date}</li>
+      <li>Room Number: ${booking.roomNumber}</li>
+      <li>Confirmation Number: ${booking.id}</li>
       </article>`)
     });
-
+    
     bookingsDisplay.insertAdjacentHTML('afterend',
-      `<h2 class="amount-spent" id="amountSpent">You have spent ${amount} on rooms!</h2>`);
-
+    `<h2 class="amount-spent" id="amountSpent">You have spent $${amount.toFixed(2)} on rooms!</h2>`);
+    
     this.hide([logInForm]);
     this.show([dashboard]);
   },
-
-  displayAvailableRooms() {
+  
+  displayAvailableRooms(roomListings, bookingsLog, logInForm) {
+    console.log('BOOKINGS', bookingsLog, 'ROOMS', roomListings)
     availableRooms.innerHTML = '';
-    let roomsToDisplay = bookingsLog.getAvailableRooms(dateInput.value, roomListings);
+    let date = dateInput.value;
+    let roomsToDisplay = bookingsLog.getAvailableRooms(date, roomListings);
     roomsToDisplay.forEach(room => {
       availableRooms.insertAdjacentHTML('beforeend', `<article class="available-room-card">
       <h3>${room.roomType}</h3>
@@ -56,8 +53,8 @@ let domUpdates = {
     this.hide([logInForm, dashboard]);
     this.show([availableRooms]);
   },
-
-  displayBookingARoom(event) {
+  
+  displayBookingARoom(event, logInForm, dashboard, bookingARoom, backToDashBtn) {
     roomListings.find(room => {
       if (event.target.closest(room.id)) {
         // [!room.bidet] = 'No'
@@ -68,32 +65,32 @@ let domUpdates = {
           room.bidet = 'Yes'
         }
         bookingARoom.insertAdjacentHTML('afterbegin',
-          `<article class="selected-room">
-            <h3>${room.roomType}</h3>
-            <h4>Features</h4>
-            <ul>
-              <li>Bidet: ${room.bidet} </li>
-              <li>${room.numBeds} ${room.bedSize}</li>
-            </ul>
-            <h5>Cost Per Night: ${room.costPerNight}</h5>
-          </article>`)
+        `<article class="selected-room">
+        <h3>${room.roomType}</h3>
+        <h4>Features</h4>
+        <ul>
+        <li>Bidet: ${room.bidet} </li>
+        <li>${room.numBeds} ${room.bedSize}</li>
+        </ul>
+        <h5>Cost Per Night: ${room.costPerNight}</h5>
+        </article>`)
       }
     })
     this.hide([logInForm, dashboard]);
-    this.show([bookingARoom]);
+    this.show([bookingARoom, backToDashBtn]);
   },
-
-
+  
+  
   //HELPER FUNCTIONS
-
+  
   show(elements) {
       elements.forEach(element => element.classList.remove('hidden'));
     },
-
+  
   hide(elements) {
     elements.forEach(element => element.classList.add('hidden'));
-  }
-
+  },
+  
 };
 
 export default domUpdates;
