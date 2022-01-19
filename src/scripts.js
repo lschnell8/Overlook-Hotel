edb7d9152df9d3259c24e0adb54d2c38dc65b811
lschnell8 <lschnell8@gmail.com>
2022-01-19 13:23:57 -0700

    Recover pieces of code lost to the unknown

--- src/scripts.js
+++ src/scripts.js
@@ -5,57 +5,21 @@ import Customer from './classes/Customer';
 import domUpdates from './domUpdates.js';
 import './css/base.scss';
 
-//BUTTONS AND SUBMITS
+//BUTTONS, INPUTS, AND SUBMITS
 const logInForm = document.getElementById('logIn');
+const roomTypeInput = document.getElementById('typeInput');
+const roomSearchForm = document.getElementById('roomSearch');
 
 // const findAvailableRoomsBtn = document.getElementById('findAvailableRoomsBtn');
 // const backToDashBtn = document.getElementById('backToDashBtn');
-const bookMyRoomBtn = document.getElementById('bookMyRoomBtn');
-
-//INPUTS
-const roomTypeSelection = document.getElementById('typeInput');
-// const usernameInput = document.getElementById('usernameInput');
-// const passwordInput = document.getElementById('passwordInput');
-// const dateInput = document.getElementById('dateInput').value;
-const roomSearchForm = document.getElementById('roomSearch');
-// const roomTypeSelection = document.getElementById('roomTypeSelection');
-
-//PAGE VIEWS
-// const logInPage = document.getElementById('logIn');
-// const dashboard = document.getElementById('dashboard');
-// const availableRooms = document.getElementById('availableRooms');
-// const bookingARoom = document.getElementById('bookingARoom');
-
-//TEXT //Don't need these can insertAdjacent HTML afterbegin dashboard
-// const greeting = document.getElementById('greeting');
-// const amountSpent = document.getElementById('amountSpent');
-
-
-//roomTypeSelection.options[roomTypeSelection.selectedIndex].value; //This reads the value of the option in the select tag for roomType selection
-
+// const bookMyRoomBtn = document.getElementById('bookMyRoomBtn');
 
 //GLOBAL VARIABLES
 let bookingsLog, roomListings, customer;
 let currentDate = new Date().toJSON().slice(0, 10);
 let date = currentDate.split("-").join("/");
+
 //EXECUTION FUNCTIONS
-
-// const loadPage = () => {
-//   console.log(document.activeElement)
-//   console.log(usernameInput.value)
-// };
-
-// const loadData = () => {
-  // usernameInput.onblur = () => {
-  // customerInput
-  // }
-  
-  // if (usernameInput.value === '') {
-    // domUpdates.displayInputError(usernameInput)
-  // // } else if (usernameInput.value.length < 9 || > 10) {
-  // }
-// };
-
 const logIn = (event) => {
   event.preventDefault();
   if (usernameInput.value === '' || passwordInput.value === '' || passwordInput.value !== 'overlook2021') {
@@ -67,28 +31,30 @@ const logIn = (event) => {
       domUpdates.displayDashboard(customer, bookingsLog, roomListings, logInForm, date)
     }) 
   }
+  // usernameInput.innerText = '';
+  // passwordInput.innerText = '';
 };
 
 const apendAvailableRooms = (event) => {
   event.preventDefault();
-  domUpdates.displayAvailableRooms(roomListings, bookingsLog, logInForm);
+  let inputDate = dateInput.value.split('-').join('/')
+
+  domUpdates.displayAvailableRooms(logInForm, roomListings, bookingsLog, inputDate);
 };
 
 const getFilteredRooms = () => {
-  debugger;
-  let roomStyle = roomTypeSelection.value;
-  console.log(roomStyle)
+  let roomStyle = roomTypeInput.value;
+  // console.log(roomStyle)
   let separateByType = bookingsLog.getAvailableRoomsByType(roomStyle, date, roomListings);
   console.log(separateByType)
   separateByType.forEach(room => {
-    room
-    domUpdates.displayFilteredRooms();
+    domUpdates.displayFilteredRooms(room);
   });
 };
 
-const postBooking = () => {
+// const postBooking = () => {
 
-};
+// };
 
 
 //HELPER FUNCTIONS
@@ -107,8 +73,8 @@ const instantiateClassInstances = (data) => {
 logInForm.addEventListener('submit', logIn);
 // findAvailableRoomsBtn.addEventListener('click', apendAvailableRooms);
 roomSearchForm.addEventListener('submit', apendAvailableRooms)
-// roomTypeSelection.addEventListener('submit', getFilteredRooms)
-backToDashBtn.addEventListener('click', domUpdates.displayDashboard(customer, bookingsLog, roomListings, logInForm));
-bookMyRoomBtn.addEventListener('click', postBooking)
+roomTypeSelection.addEventListener('submit', getFilteredRooms)
+// backToDashBtn.addEventListener('click', domUpdates.displayDashboard(customer, bookingsLog, roomListings, logInForm));
+// bookMyRoomBtn.addEventListener('click', postBooking)
 
-export default { logInForm, date, currentDate, roomTypeSelection}
+export default { logInForm, date, currentDate, roomTypeSelection, bookingsLog, roomListings, customer}
