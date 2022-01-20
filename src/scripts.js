fce1c404bb6819e6850dd44ec6e722865ba282a6
lschnell8 <lschnell8@gmail.com>
2022-01-20 09:29:35 -0700

    Create helper functions to utilize cleaner SRP code

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-import { fetchApiData } from './apiCalls';
+import { fetchApiData, submitBooking } from './apiCalls';
 import BookingsLog from './classes/BookingsLog';
 import RoomListings from './classes/RoomListings';
 import Customer from './classes/Customer';
@@ -7,13 +7,11 @@ import './css/base.scss';
 
 //BUTTONS, INPUTS, AND SUBMITS
 const logInForm = document.getElementById('logInForm');
-const typeInput = document.getElementById('typeInput').value.onclick();
-const roomTypeSelection = document.getElementById('roomTypeSelection')
-// const roomSearchForm = document.getElementById('roomSearchForm');
-
+const roomTypeSelection = document.getElementById('roomTypeSelection');
+const filterByTypeBtn = document.getElementById('filterByType');
 const findAvailableRoomsBtn = document.getElementById('findAvailableRoomsBtn');
-// const backToDashBtn = document.getElementById('backToDashBtn');
-// const bookMyRoomBtn = document.getElementById('bookMyRoomBtn');
+const backToDashBtn = document.getElementById('backToDashBtn');
+const bookMyRoomBtn = document.getElementById('bookMyRoomBtn');
 
 //GLOBAL VARIABLES
 let bookingsLog, roomListings, customer;
@@ -21,68 +19,71 @@ let currentDate = new Date().toJSON().slice(0, 10);
 let date = currentDate.split("-").join("/");
 
 //EXECUTION FUNCTIONS
-
 const logIn = (event) => {
   event.preventDefault()
-  // let userValue = usernameInput.value.split('r')[1]
-  // if (userValue > 50 || 0 > userValue || passwordInput.value !== 'overlook2021') {
-    // domUpdates.displayLoginInputError();
-    // } else {
-      let id = usernameInput.value.split('r')[1];
-      getData(id)
-      .then(data => {
-        domUpdates.displayDashboard(customer, bookingsLog, roomListings, logInForm, date)
-      }) 
-      // }
-      // usernameInput.innerText = '';
-      // passwordInput.innerText = '';
-    };
+  let userValue = usernameInput.value.split('r')[1]
+  if (userValue > 50 || 0 > userValue || passwordInput.value !== 'overlook2021') {
+    domUpdates.displayLoginInputError();
+  } else {
+  let id = usernameInput.value.split('r')[1];
+    getData(id)
+    .then(data => {
+      renderDashboard();
+    }) 
+  }    
+};
     
-  //HELPER FUNCTIONS
-  const getData = (id) => {
-    return Promise.all([fetchApiData('bookings'), fetchApiData('rooms'), fetchApiData('customers', id)]).then(data => instantiateClassInstances(data))
-  };
+const getFilteredRooms = (event) => {
+  event.preventDefault();
+  let dropDownSelection = getInputValue();
+  console.log('S-47 gFR Select Option Value', typeInput.value, 'variable', dropDownSelection);
+  let separateByType = bookingsLog.getAvailableRoomsByType(dropDownSelection, date, roomListings);
+  console.log('S-50 gFR separateByType', separateByType);
+  separateByType.forEach(room => {
+    domUpdates.displayFilteredRooms(room);
+  });
+  domUpdates.showFilteredRooms();
+};
+
+const postBooking = () => {
   
-  const instantiateClassInstances = (data) => {
-    bookingsLog = new BookingsLog(data[0].bookings);
-    roomListings = new RoomListings(data[1].rooms);
-    customer = new Customer(data[2]);
-  };
+};
+
+const renderDashboard = () => {
+  domUpdates.displayDashboard(customer, bookingsLog, roomListings, date)
+  domUpdates.showDashboard();
+};
+
+//HELPER FUNCTIONS
+const getData = (id) => {
+  return Promise.all([fetchApiData('bookings'), fetchApiData('rooms'), fetchApiData('customers', id)]).then(data => instantiateClassInstances(data))
+};
+
+const instantiateClassInstances = (data) => {
+  bookingsLog = new BookingsLog(data[0].bookings);
+  roomListings = new RoomListings(data[1].rooms);
+  customer = new Customer(data[2]);
+};
 
 const apendAvailableRooms = (event) => {
   event.preventDefault();
-  domUpdates.displayAvailableRooms(logInForm, roomListings, bookingsLog);
+  domUpdates.displayAvailableRooms(roomListings, bookingsLog);
+  domUpdates.showAvailableRooms();
 };
 
-// const getInputValue = () => {
-//   let roomStyle = typeInput.value;
-// };
-
-const getFilteredRooms = (event) => {
-  event.preventDefault();
-  // select = document.getElementById('language');
-  console.log('S-47 gFR Select Option Value', typeInput);
-  // let typeInput = typeInput.value;
-  let separateByType = bookingsLog.getAvailableRoomsByType(typeInput, date, roomListings);
-  console.log('S-50 gFR separateByType', separateByType);
-  separateByType.forEach(room => {
-    domUpdates.displayFilteredRooms(room)
-  });
+const getInputValue = () => {
+  const typeInput = document.getElementById('typeInput');
+  let selection = typeInput.options[typeInput.selectedIndex].value;
+    return selection
 };
-
-// const postBooking = () => {
-
-// };
-
-
 
 
 //EVENT LISTENERS
 logInForm.addEventListener('submit', logIn);
 findAvailableRoomsBtn.addEventListener('click', apendAvailableRooms);
-// roomSearchForm.addEventListener('submit', apendAvailableRooms);
-roomTypeSelection.addEventListener('submit', getFilteredRooms);
-// backToDashBtn.addEventListener('click', domUpdates.displayDashboard(customer, bookingsLog, roomListings, logInForm));
-// bookMyRoomBtn.addEventListener('click', postBooking);
+bookMyRoomBtn.addEventListener('click', postBooking);
+backToDashBtn.addEventListener('click', renderDashboard);
+roomTypeSelection.addEventListener('submit', getInputValue)
+filterByTypeBtn.addEventListener('click', getFilteredRooms);
 
 export { logInForm, date, currentDate, bookingsLog, roomListings, customer }
